2d97c5b7018e15ead5aa18daf9905ca80fa19c3e
TerryZ <terry5@foxmail.com>
2024-07-14 16:00:20 +0800

    feat: 统一函数处理各 method type

--- src/http.js
+++ src/http.js
@@ -1,62 +1,25 @@
 import { prototype } from './settings'
-import { isSessionTimeout } from './storage'
-import { execute } from './handle'
+import { isSessionTimeout as isSessionInvalid } from './storage'
+import { httpDataRequest } from './handle'
 import { handleUrl, timeConvert, buildSettings, checkEnvironment } from './utils'
-import { defaultOptions, method } from './constants'
+import { defaultOptions } from './constants'
 
-/**
- * http data request
- */
-class Http {
+export function useHttpDataRequest (setupOptions) {
+  let cancelControl = new AbortController()
+  const options = Object.assign({}, defaultOptions, setupOptions)
+  // user authorization expires-in millisecond
+  options.expiresInMillisecond = timeConvert(options.expiresIn)
+  // create a new axios instance
+  const http = prototype(options)
   /**
-   * Creates an instance of Http
-   *
-   * @param {object} options - plugin config options
-   *
-   * {
-   *   # 访问地址的基础前缀，若实际访问地址是以 http/https 开始，则使用实际地址，忽略前缀设定
-   *   baseUrl: string,
-   *   # 执行刷新 refresh token 的 api 位置
-   *   refreshUrl: string,
-   *   # 执行刷新时传递 refresh token 使用的请求参数名，默认 `refreshToken`
-   *   paramRefreshToken: string,
-   *
-   *   # 登录会话有效时长，单位为分钟
-   *   # default: 0
-   *   expiresIn: number,
-   *
-   *   # 数据字段适配模型，用于适配服务器返回的数据字段，大多数情况下，应要求服务器按前端要求返
-   *   # 回指定数据格式，但部分历史项目需要进行适配，此处提供了可兼容旧数据格式的方式
-   *   # default: 'accessToken'
-   *   keyAccessToken: string,
-   *   # default: 'expiresIn'
-   *   keyExpiresIn: string,
-   *
-   *   # 单次请求超时时长，单位为毫秒，
-   *   # default: 20000
-   *   timeout: number,
-   *
-   *   # 业务错误/用户登录授权失效/系统错误时的相关信息展示
-   *   # message {string} 异常信息文本
-   *   # type    {string} 异常类型（常量）
-   *   exception: function (message, type) {}
-   * }
-   *
-   * @see constants.js defaultOptions content
-   * @constructor
-   * @memberof Http
+   * Cancel controller factory
    */
-  constructor (options) {
-    this.options = Object.assign({}, defaultOptions, options)
-    // user authorization expires-in millisecond
-    this.options.expiresInMillisecond = timeConvert(this.options.expiresIn)
-    // axios instance
-    this.http = prototype(this.options)
-    // axios cancel token object
-    // this.cancelTokenSource = CancelToken.source()
-    this.cancelControl = new AbortController()
+  function getCancelControl () {
+    if (!cancelControl || cancelControl.signal.aborted) {
+      cancelControl = new AbortController()
+    }
+    return cancelControl
   }
-
   /**
    * Execute http request
    *
@@ -65,11 +28,8 @@ class Http {
    * @param {object} userSettings - optional
    *
    * @returns {function} a promise type function
-   * @memberof Http
    */
-  fetch (url, data, userSettings) {
-    const { http, options } = this
-
+  function fetch (url, data, userSettings) {
     // check parameters, options and environment
     const result = checkEnvironment(url, options)
     if (result) {
@@ -77,49 +37,38 @@ class Http {
     }
 
     const settings = buildSettings(handleUrl(url, options.baseUrl), data, userSettings)
-    // settings.cancelToken = this.cancelTokenSource.token
-    settings.signal = this.cancelControl.signal
+    settings.signal = getCancelControl().signal
 
-    return execute(http, options, settings)
+    return httpDataRequest(http, settings, options)
   }
-
   /**
    * Cancel all of the requests
-   *
-   * @memberof Http
    */
-  cancel () {
-    // this.cancelTokenSource.cancel()
-    // generate a new cancel token source
-    // this.cancelTokenSource = CancelToken.source()
-    this.cancelControl.abort()
-    this.cancelControl = new AbortController()
+  function cancel () {
+    // console.warn('cancel all requests')
+    cancelControl.abort()
   }
-
   /**
    * Check if user authorization session timeout
    *
    * @returns {boolean}
-   * @memberof Http
    */
-  isSessionTimeout () {
-    return isSessionTimeout(this.options.expiresInMillisecond)
+  function isSessionTimeout () {
+    return isSessionInvalid(options.expiresInMillisecond)
+  }
+  function fetchApplyMethod (originalArguments, method) {
+    const [url, data, setting] = originalArguments
+    return fetch(url, data, { ...setting, method })
+  }
+
+  return {
+    http: fetch,
+    cancel,
+    isSessionTimeout,
+    get: (...args) => fetchApplyMethod(args, 'GET'),
+    post: (...args) => fetchApplyMethod(args, 'POST'),
+    put: (...args) => fetchApplyMethod(args, 'PUT'),
+    patch: (...args) => fetchApplyMethod(args, 'PATCH'),
+    del: (...args) => fetchApplyMethod(args, 'DELETE')
   }
 }
-
-/**
- * Generate http methods(get, post, put, patch, delete) function in Http
- * class, and the function will setup the `method` attribute to the same
- * with function name by force
- *
- * @returns object(Promise)
- * @memberof Http
- */
-Object.values(method).forEach(type => {
-  Http.prototype[type] = function (url, data, userSettings) {
-    const setting = { ...userSettings, method: type }
-    return this.fetch(url, data, setting)
-  }
-})
-
-export default Http
