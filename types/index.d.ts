5f8b49755b7b3f083e4213bee7a7aef0aac6390f
Terry <terry5@foxmail.com>
2024-07-25 16:28:30 +0800

    feat: dts 文档

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,171 +1,54 @@
-declare type HttpExceptionType = 'exception-business'
-  | 'exception-auth-invalid'
-  | 'exception-system'
+import { AxiosRequestConfig } from 'axios'
+import { HttpDataRequestOptions } from './type'
+
+export * from './type'
 
 /**
- * http 初始化设置参数模型
+ * Data request
+ * @param url request api address
+ * @param data the data that send to server
+ * @param options axios request options
  */
-declare interface HttpOptions {
+declare function httpDataRequest(
+  url: string,
+  data: any,
+  options: AxiosRequestConfig
+): Promise<any>
+
+interface HttpDataRequestMethods {
+  http: typeof httpDataRequest
   /**
-   * 访问地址的基础前缀，若实际访问地址是以 http/https 开始，则使用实际地址，忽略前缀设定
-   *
-   * @default `/`
+   * Http request with get method
    */
-  baseUrl: string
+  get: typeof httpDataRequest
   /**
-   * 执行刷新 refresh token 的 api 位置
-   *
-   * @default `/auth/refresh-token`
+   * Http request with post method
    */
-  refreshUrl?: string
+  post: typeof httpDataRequest
   /**
-   * 执行刷新时传递 refresh token 使用的请求参数名
-   *
-   * @default `refreshToken`
+   * Http request with put method
    */
-  paramRefreshToken?: string
+  put: typeof httpDataRequest
   /**
-   * 登录会话有效时长，单位为分钟
-   *
-   * @default 0
+   * Http request with patch method
    */
-  expiresIn?: number
+  patch: typeof httpDataRequest
   /**
-   * 数据字段适配模型，用于适配服务器返回的数据字段，大多数情况下，应按照标准协议要求返
-   * 回指定数据格式，但部分历史项目需要进行适配，此处提供了可兼容旧数据格式的方式
-   *
-   * @default `accessToken`
+   * Http request with delete method
    */
-  keyAccessToken?: string
+  del: typeof httpDataRequest
   /**
-   * 会话超时时间字段名
-   *
-   * @default `expiresIn`
+   * Cancel all current request
    */
-  keyExpiresIn?: string
+  cancel: () => void
   /**
-   * 单次请求超时时间，单位：毫秒
-   *
-   * @default 20000
+   * Check if session timeout
    */
-  timeout?: number
-  /**
-   * 异常状态信息处理
-   * 业务错误/用户登录授权失效/系统错误时的相关信息展示
-   *
-   * @param {string} message - 错误/异常信息内容
-   * @param {number} type - 错误/异常类型
-   */
-  exception(
-    message: string,
-    type: HttpExceptionType
-  ): void
+  isSessionTimeout: () => boolean
 }
 
 /**
- * http 数据请求
+ * Create customized data request methods for web projects
+ * @param options
  */
-export module '@plugins-core/http' {
-  /**
-   * http 数据请求对象
-   */
-  export class Http {
-    /**
-     * 构造函数
-     *
-     * @param {HttpOptions} options - 插件初始化参数集
-     * @constructor
-     */
-    constructor (options: HttpOptions): void
-    /**
-     * 执行 http 数据请求
-     *
-     * @param {string} url - 数据请求位置
-     * @param {object=} data - 请求数据
-     * @param {object=} userSettings - axios 原生请求设置参数
-     *
-     * @returns {function} a promise type function
-     * @deprecated 该对象不再推荐使用，仅作为旧项目兼容
-     */
-    http (url: string, data?: object, userSettings?: object): Promise
-    /**
-     * 使用 get 方式执行 http 数据请求
-     *
-     * @param {string} url - 数据请求位置
-     * @param {object=} data - 请求数据
-     * @param {object=} userSettings - axios 原生请求设置参数
-     *
-     * @returns {function} a promise type function
-     */
-    get (url: string, data?: object, userSettings?: object): Promise
-    /**
-     * 使用 post 方式执行 http 数据请求
-     *
-     * @param {string} url - 数据请求位置
-     * @param {object=} data - 请求数据
-     * @param {object=} userSettings - axios 原生请求设置参数
-     *
-     * @returns {function} a promise type function
-     */
-    post (url: string, data?: object, userSettings?: object): Promise
-    /**
-     * 使用 put 方式执行 http 数据请求
-     *
-     * @param {string} url - 数据请求位置
-     * @param {object=} data - 请求数据
-     * @param {object=} userSettings - axios 原生请求设置参数
-     *
-     * @returns {function} a promise type function
-     */
-    put (url: string, data?: object, userSettings?: object): Promise
-    /**
-     * 使用 patch 方式执行 http 数据请求
-     *
-     * @param {string} url - 数据请求位置
-     * @param {object=} data - 请求数据
-     * @param {object=} userSettings - axios 原生请求设置参数
-     *
-     * @returns {function} a promise type function
-     */
-    patch (url: string, data?: object, userSettings?: object): Promise
-    /**
-     * 使用 delete 方式执行 http 数据请求
-     *
-     * @param {string} url - 数据请求位置
-     * @param {object=} data - 请求数据
-     * @param {object=} userSettings - axios 原生请求设置参数
-     *
-     * @returns {function} a promise type function
-     */
-    del (url: string, data?: object, userSettings?: object): Promise
-    /**
-     * 取消所有当前正在执行的请求
-     */
-    cancel (): void
-    /**
-     * 用户认证会话状态是否失效
-     *
-     * @returns {boolean}
-     */
-    isSessionTimeout (): void
-  }
-  /**
-   * Setup the functions need to export
-   *
-   * @param {object} instance - HttpRequest 实例对象
-   * @returns {object} http 函数集及工具集
-   */
-  export function httpSetup (instance: object): void
-  /**
-   * 业务异常状态
-   */
-  export const EXCEPTION_BUSINESS: HttpExceptionType
-  /**
-   * 用户身份认证状态失效
-   */
-  export const EXCEPTION_AUTH_INVALID: HttpExceptionType
-  /**
-   * 系统错误（底层服务异常）
-   */
-  export const EXCEPTION_SYSTEM: HttpExceptionType
-}
+export declare function useHttpDataRequest(options: HttpDataRequestOptions): HttpDataRequestMethods
---
+++ types/type.d.ts
@@ -0,0 +1,91 @@
+
+export declare type EXCEPTION_BUSINESS = 'exception-business'
+export declare type EXCEPTION_AUTH_INVALID = 'exception-auth-invalid'
+export declare type EXCEPTION_SYSTEM = 'exception-system'
+export declare type EXCEPTION_CANCELLED = 'exception-cancelled'
+
+export declare type EXCEPTION_TYPE = EXCEPTION_BUSINESS | EXCEPTION_AUTH_INVALID | EXCEPTION_SYSTEM
+
+export declare interface HttpDataRequestOptions {
+  /**
+   * Plugin language
+   * @default `en`
+   */
+  language: string
+  /**
+   * Base url path
+   * @default `/`
+   */
+  baseUrl: string
+  /**
+   * The url used to refresh new access token
+   * @default `/auth/refresh-token`
+   */
+  refreshUrl: string
+  /**
+   * @default 0
+   */
+  expiresIn: number
+  /**
+   * Specifies the number of milliseconds before the request times out
+   * @default 10000
+   */
+  timeout: number
+  /**
+   * Customize the key name of the data node
+   */
+  keys: {
+    /**
+     * Access data node name
+     * @default `access`
+     */
+    dataSet: string
+    /**
+     * Access token property name
+     * @default `accessToken`
+     */
+    accessToken: string
+    /**
+     * Refresh token property name
+     * @default `refreshToken`
+     */
+    refreshToken: string
+    /**
+     * Token expires in property name
+     * @default `expiresIn`
+     */
+    expiresIn: string
+    /**
+     * The property name in the request body to pass refresh token
+     */
+    paramRefreshToken: string
+    /**
+     * The property name of the request headers to pass access token
+     */
+    header: string
+  }
+  /**
+   * Customize the status code of the response
+   */
+  statuses: {
+    /**
+     * Success status code
+     * @default 0
+     */
+    success: number
+    /**
+     * Access token invalid status code
+     * @default 10
+     */
+    invalidAccessToken: number
+    /**
+     * Refresh token invalid status code
+     * @default 11
+     */
+    invalidRefreshToken: number
+  }
+  /**
+   * Request exception handle
+   */
+  exception: (message: string, type: EXCEPTION_TYPE) => void
+}
